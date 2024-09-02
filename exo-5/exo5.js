a69c588fe39e31a46f018fbef8d919ec45395e2a
Camille RIQUET <camille.riquet@Digital-Grenoble.local>
2024-09-02 15:49:41 +0200

    Ex 5 - Les boucles

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,27 @@
 // you can write js here
-
 console.log('exo-5');
+input = prompt("Quel phrase voulez vous traduire en baleine?");
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+
+// 1ere solution
+let resultArray = [];
+for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+    if (vowels.indexOf(input[i]) !== -1) {
+        resultArray.push(input[i]);
+    }
+}
+console.log(resultArray.join('').toUpperCase())
+
+// 2eme solution
+let resultArray2 = [];
+for (let i = 0; i < input.length; i++) {
+    console.log(input[i]);
+    for (let j = 0; j < vowels.length; j++) {
+        if (input[i] === vowels[j]) {
+            resultArray2.push(input[i]);
+        }
+    }
+}
+console.log(resultArray2.join('').toUpperCase())
