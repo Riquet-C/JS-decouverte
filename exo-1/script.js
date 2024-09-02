79602b7e16eb664f8f788c8dd130979bf2645ce2
Camille RIQUET <camille.riquet@Digital-Grenoble.local>
2024-09-02 11:26:45 +0200

    Les variables

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,7 @@
 // you can write js here
-console.log('hello from file');
+var kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+let celsius = kelvins - 273; /* prends la valeur de kelvins et lui retire 273 */
+let fahrenheits = Math.floor(celsius * (9/5) + 32) /* permet d'obtenir un nombre entier */
+console.log('La température en celsus est : ' + kelvins);
+console.log('La température en celsus est : ' + celsius);
+console.log('La température en fahrenheits est : ' + fahrenheits);
