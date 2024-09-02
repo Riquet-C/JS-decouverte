f477f0641ddd9c2ba493e6397fb7e33a8329e0d1
Camille RIQUET <camille.riquet@Digital-Grenoble.local>
2024-09-02 14:36:08 +0200

    Ex 4 - Les tableaux

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,11 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let deleteLast = secretMessage.pop();
+let addWorld = secretMessage.push('to', 'program');
+let pos = secretMessage.indexOf("easily");
+secretMessage[6] = 'right';
+let deleteFirst = secretMessage.shift();
+let newFirst = secretMessage.unshift('Programming');
+let removedItem = secretMessage.splice(5, 5, 'know');
+console.log(secretMessage.join(' '));
