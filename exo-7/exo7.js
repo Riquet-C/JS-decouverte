b1f3a6549b1b225599186dff1f9d8c920ba05451
Camille RIQUET <camille.riquet@Digital-Grenoble.local>
2024-09-04 16:37:32 +0200

    data 2

--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -14,16 +14,25 @@ let tradType = {
 }
 
 // boucle pour ajouter le type traduit à chaque produit dans jsonDatas
-// for (let item of jsonDatas["type"]) {
-//     item["type traduit"] = tradType[item["type"]];
-// }
+for (let categories of jsonDatas) {
+    let type = categories["type"];
+    let items = categories["items"];
+    for (let item of items) {
+        item["type traduit"] = tradType[type];
+    }
+}
 
 //creation card pour produit
-function createCard(objet) {
+function createCard(objet, type) {
+    let descriptionHtml = '';
+    if (objet['description']) {
+        descriptionHtml = '<li> Description: ' + objet['description'] + '</li>';
+    }
+
     return '<h2>' + objet['name'] + '</h2>' +
         '<ul>' +
-        '<li> Type: ' + objet['type'] + '</li>' +
-        '<li> Description: ' + objet['description'] + '</li>' +
+        '<li> Type: ' + type + '</li>' +
+        descriptionHtml +
         '<li> Prix: ' + objet['price'] + '</li>' +
         '<li> Quantité disponible: ' + objet['quantity'] + '</li>' +
         '</ul>';
@@ -33,17 +42,23 @@ function createCard(objet) {
 function display(elements_to_display) {
     const productDiv = document.getElementById("mes_produits");
     productDiv.innerHTML = "";
-    for (let product of elements_to_display) {
-        productDiv.innerHTML += createCard(product.items)
-    }
+    let type = elements_to_display["type"];
+    elements_to_display.forEach((article) => {
+        productDiv.innerHTML += createCard(article, type)
+    })
 }
 
 function updateResults(recherche) {
+    const productDiv = document.getElementById("mes_produits");
     let result = cherche(recherche);
+    let type = result["type"];
+
+    productDiv.innerHTML = "";
+
     if (checkBox.checked) {
         result = available(result);
     }
-    display(result);
+    display(result)
 }
 
 // function recherche et dispo
@@ -52,7 +67,8 @@ function cherche(query) {
     //boucle pour afficher les produits de la catégorie renseigner
     for (let i = 0; i < jsonDatas.length; i++) {
         if (jsonDatas[i]['type'].includes(query)) {
-            categorie.push(jsonDatas[i]);
+            categorie = categorie.concat(jsonDatas[i]["items"]);
+            categorie["type"] = jsonDatas[i]['type'];
         }
     }
     return categorie
@@ -62,16 +78,29 @@ function available(listeCategorie) {
     let availableProduct = [];
     for (let i = 0; i < listeCategorie.length; i++) {
         if (listeCategorie[i]['quantity'] > 0) {
+            console.log(listeCategorie)
             availableProduct.push(listeCategorie[i]);
+            availableProduct["type"] = listeCategorie['type'];
         }
     }
     return availableProduct;
 }
 
-
 //affichage
 // au chargement initial de la page
-display(jsonDatas);
+function start(data) {
+    const productDiv = document.getElementById("mes_produits");
+    for (let categories of data) {
+        let type = categories["type"];
+        let items = categories["items"];
+        for (let item of items) {
+            productDiv.innerHTML += createCard(item, type)
+        }
+    }
+}
+
+start(jsonDatas);
+
 
 // si une recherche est faite
 const input = document.getElementById("input");
@@ -115,11 +144,27 @@ priceDown.addEventListener("click", function (event) {
 const form = document.getElementById('form');
 form.addEventListener("submit", function (event) {
     event.preventDefault();
+    // recup les données
     const name = document.getElementById("formName").value;
     const type = document.getElementById("formType").value;
     const description = document.getElementById("formDescription").value;
     const price = parseInt(document.getElementById("formPrice").value);
     const quantity = parseInt(document.getElementById("formQuantity").value);
-    jsonDatas.push({"name" : name, "type": type, "description" : description, "price" : price, "quantity" : quantity});
-    display(jsonDatas);
+
+    // crée produit
+    const newProduct = {
+        "name": name, "description": description, "price": price, "quantity": quantity, "contact": {
+            "lastName": "Dubois",
+            "firstName": "Martin",
+            "address": "1 Grande Rue 74000 Annecy"
+        }
+    }
+
+    //ajout à jsonDatas
+    for (let i = 0; i < jsonDatas.length; i++) {
+        if (type === jsonDatas[i].type) {
+            jsonDatas[i].items.push(newProduct);
+        }
+        console.log(jsonDatas[i]);
+    }
 })
